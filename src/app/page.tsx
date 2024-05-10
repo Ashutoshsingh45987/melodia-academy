6dbd5ab8d73c47941d1408d74a5bd2423842ca36
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-05-10 18:15:37 +0530

    Testimonial card with infinite moving cards ..grid background.. infinite scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import FeaturedCoursed from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChoosesUs from "@/components/WhyChoosesUs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.93]">
+    <main className="min-h-screen bg-black/[0.96] antialiased ">
       <HeroSection/>
       <FeaturedCoursed/>
+      <WhyChoosesUs/>
+      <TestimonialCards/>
     </main>
   );
 }
